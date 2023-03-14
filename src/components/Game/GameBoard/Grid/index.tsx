899da2b60a72cf909ccb41b2a91125fa3513fff9
chris Chen <59293007+githubucl@users.noreply.github.com>
2023-03-15 12:03:21 +1300

    render different image depending on state

--- src/components/Game/GameBoard/Grid/index.tsx
+++ src/components/Game/GameBoard/Grid/index.tsx
@@ -3,8 +3,26 @@ import { TGridInfo } from "../../../../types";
 
 //style
 import "./Grid.scss";
-const Grid = ({ position, ship, fired }: TGridInfo): JSX.Element => {
-  return <div className="Grid"></div>;
+
+type TGridProps = TGridInfo & {
+  onGridClick: (clickedPosition: string) => void;
+};
+const Grid = ({
+  position,
+  ship,
+  fired,
+  onGridClick,
+}: TGridProps): JSX.Element => {
+  return (
+    <div className="Grid" onClick={() => onGridClick(position)}>
+      {fired &&
+        (ship === "none" ? (
+          <img className="Grid__image" src="/assets/Miss.png" alt="MISS" />
+        ) : (
+          <img className="Grid__image" src="/assets/Hit.png" alt="HIT" />
+        ))}
+    </div>
+  );
 };
 
 export default Grid;
